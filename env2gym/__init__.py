014c7a852fd5271f7cec79f7939861b846244585
Your Name <you@example.com>
2020-05-29 00:15:09 -0700

    make the mock setup for gym2gym

--- env2gym/__init__.py
+++ env2gym/__init__.py
@@ -2,7 +2,7 @@ import gym
 from gym.envs.registration import register
 
 
-def make(
+def dmc_make(
         domain_name,
         task_name,
         seed=1,
@@ -17,6 +17,7 @@ def make(
         time_limit=None,
         channels_first=True
 ):
+
     env_id = 'dmc_%s_%s-v1' % (domain_name, task_name)
 
     if from_pixels:
@@ -37,6 +38,54 @@ def make(
             kwargs=dict(
                 domain_name=domain_name,
                 task_name=task_name,
+                task_kwargs=task_kwargs,
+                environment_kwargs=environment_kwargs,
+                visualize_reward=visualize_reward,
+                from_pixels=from_pixels,
+                height=height,
+                width=width,
+                camera_id=camera_id,
+                frame_skip=frame_skip,
+                channels_first=channels_first,
+            ),
+            max_episode_steps=max_episode_steps,
+        )
+    return gym.make(env_id)
+
+def gym_make(
+        env_name,
+        seed=1,
+        visualize_reward=True,
+        from_pixels=False,
+        height=84,
+        width=84,
+        camera_id=0,
+        frame_skip=1,
+        episode_length=1000,
+        environment_kwargs=None,
+        time_limit=None,
+        channels_first=True
+):
+
+    env_id = env_name
+
+    if from_pixels:
+        assert not visualize_reward, 'cannot use visualize reward when learning from pixels'
+
+    # shorten episode length
+    max_episode_steps = (episode_length + frame_skip - 1) // frame_skip
+
+    if not env_id in gym.envs.registry.env_specs:
+        task_kwargs = {}
+        if seed is not None:
+            task_kwargs['random'] = seed
+        if time_limit is not None:
+            task_kwargs['time_limit'] = time_limit
+        register(
+            id=env_id,
+            entry_point='env2gym.wrappers:GYMWrapper',
+            kwargs=dict(
+                env_name=env_name,
                 task_kwargs=task_kwargs,
                 environment_kwargs=environment_kwargs,
                 visualize_reward=visualize_reward,
