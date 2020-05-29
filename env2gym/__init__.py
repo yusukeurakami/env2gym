4f983b4279d08667e27cebccd3088771ef62b51e
Your Name <you@example.com>
2020-05-28 23:28:15 -0700

    initial commit

--- env2gym/__init__.py
+++ env2gym/__init__.py
@@ -33,7 +33,7 @@ def make(
             task_kwargs['time_limit'] = time_limit
         register(
             id=env_id,
-            entry_point='dmc2gym.wrappers:DMCWrapper',
+            entry_point='env2gym.wrappers:DMCWrapper',
             kwargs=dict(
                 domain_name=domain_name,
                 task_name=task_name,
--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(
-    name='dmc2gym',
+    name='env2gym',
     version='1.0.0',
-    author='Denis Yarats',
-    description=('a gym like wrapper for dm_control'),
+    author='Yusuke Urakami based on the code of Denis Yarats',
+    description=('a gym like wrapper for dm_control and else env'),
     license='',
-    keywords='gym dm_control openai deepmind',
+    keywords='gym dm_control openai deepmind bullet',
     packages=find_packages(),
     install_requires=[
         'gym',
